a0f60355f26d50f5c26589ec51880f024376ecbd
Bhavesh Joshi <bjwrkstuff@gmail.com>
2026-02-26 21:26:27 +0530

    wrote programs including bas arithmetic operators

--- operators/script.js
+++ operators/script.js
@@ -1,6 +1,32 @@
 // Arithmetic Operators
 var x = 5;
 var y = 10;
+var lb = "<br/>";
 
 var result;
+result = a + b ;
+document.write(result);
+document.write(lb);
+
+result = a - b;
+document.write(result);
+document.write(lb);
+
+result = a * b;
+document.write(result);
+document.write(lb);
+
+result = a / b;
+document.write(result);
+document.write(lb);
+
+result = a % b;
+document.write(result);
+document.write(lb);
+
+
+
+
+
+
 
