01d63ebf91e201d2bfdac02afb60a6a018f5780a
Bhavesh Joshi <bjwrkstuff@gmail.com>
2026-02-27 12:20:45 +0530

    functions

---
+++ functions/script.js
@@ -0,0 +1,14 @@
+/*
+    function functionName(parameters){
+    //block of code
+    }
+*/
+
+function greet(){
+
+    alert("Hello World!")
+}
+
+function sayHello(name,age){
+    document.write(name + "is "+ age + " years old.")
+}
