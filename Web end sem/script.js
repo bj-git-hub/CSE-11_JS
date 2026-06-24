c39f850fc4c490a3f2ca11c3c775dbf5cab3159b
Bhavesh Joshi <bjwrkstuff@gmail.com>
2026-06-24 14:01:37 +0530

    End Semester Practical

---
+++ Web end sem/script.js
@@ -0,0 +1,28 @@
+function calculateResult() {
+
+    let s1 = Number(document.getElementById("sub1").value);
+    let s2 = Number(document.getElementById("sub2").value);
+    let s3 = Number(document.getElementById("sub3").value);
+    let s4 = Number(document.getElementById("sub4").value);
+    let s5 = Number(document.getElementById("sub5").value);
+
+    let total = s1 + s2 + s3 + s4 + s5;
+    let percentage = total / 5;
+
+    let result;
+
+    if (percentage >= 75) {
+        result = "Distinction";
+    } else if (percentage >= 60) {
+        result = "First Class";
+    } else if (percentage >= 40) {
+        result = "Second Class";
+    } else {
+        result = "Fail";
+    }
+
+    document.getElementById("result").innerHTML =
+        "Total Marks: " + total +
+        "<br>Percentage: " + percentage.toFixed(2) + "%" +
+        "<br>Result: " + result;
+}
